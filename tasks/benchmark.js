f1d0b42858ef10064455d7249ece6b6000f95d9b
Fabio Spampinato <spampinabio@gmail.com>
2023-02-12 21:36:56 +0000

    Updated dependencies

--- tasks/benchmark.js
+++ tasks/benchmark.js
@@ -7,8 +7,7 @@ import random from '../dist/index.js';
 /* MAIN */
 
 benchmark.defaultOptions = Object.assign ( benchmark.defaultOptions, {
-  iterations: 100000,
-  log: 'compact'
+  iterations: 100_000
 });
 
 benchmark ({
